b4ff1c26cba69d8f304ab5d99505ea9c05ed8200
Ryan Carnovsky <rcarnovsky@gmail.com>
2020-04-11 14:27:21 -0400

    Add passport integration

--- server/app.js
+++ server/app.js
@@ -3,8 +3,11 @@ app = express();
 const session = require('express-session');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
 const cors = require('cors');
 const morgan = require('morgan');
+const User = require('./models/user');
 
 const sess = {
     secret: "YEET THIS WILL BE YEETED LATER",
@@ -16,6 +19,12 @@ app.use(session(sess));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(morgan('combined'));
 app.use(cors());
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 mongoose.connect('mongodb://localhost/ecommerce');
 
